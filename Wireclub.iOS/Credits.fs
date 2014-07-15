5f29982c0b3bd027e3f613531dc79a998cf5ef76
Christopher Hrica <chris.hrica@gmail.com>
2014-07-14 17:01:37 -0700

    Load a basic list of products

--- Wireclub.iOS/Credits.fs
+++ Wireclub.iOS/Credits.fs
@@ -11,10 +11,13 @@ open System.Text.RegularExpressions
 
 open MonoTouch.Foundation
 open MonoTouch.UIKit
+open MonoTouch.StoreKit
+
 open Wireclub.Models
 open Wireclub.Boundary
 open Wireclub.Boundary.Chat
 open Wireclub.Boundary.Models
+
 open ChannelEvent
 
 open Utility
@@ -24,5 +27,46 @@ open Utility
 type CreditsViewController () =
     inherit UIViewController ("CreditsViewController", null)
 
+    let localizedPrice (product:SKProduct) =
+        let formatter = new NSNumberFormatter()
+        formatter.FormatterBehavior <- NSNumberFormatterBehavior.Version_10_4
+        formatter.NumberStyle <- NSNumberFormatterStyle.Currency
+        formatter.Locale <- product.PriceLocale
+        formatter.StringFromNumber(product.Price)
+
+
+    let productsRequestDelegate = { 
+        new SKProductsRequestDelegate () with
+            override this.ReceivedResponse (request, response) =
+                for product in response.Products do
+                    printfn "LocalizedDescription: %s" product.LocalizedDescription
+                    printfn "LocalizedTitle: %s" product.LocalizedTitle
+                    printfn "ProductIdentifier: %s" product.ProductIdentifier
+                    printfn "ProductIdentifier: %s" (localizedPrice product)
+
+                for product in response.InvalidProducts do
+                    Logger.log (Exception(sprintf "[StoreKit] InvalidProduct - %s" product))
+
+            override this.RequestFailed (request, error) = Logger.log (Exception( error.Description))
+        }
+
     [<Outlet>]
     member val Table: UITableView = null with get, set
+
+    override this.ViewDidLoad () =
+        let ids =
+            NSSet.MakeNSObjectSet<NSString>(
+                [|
+                    new NSString("wiredev.credits.1075")
+                    new NSString("wiredev.credits.12000")
+                    new NSString("wiredev.credits.2200")
+                    new NSString("wiredev.credits.500")
+                    new NSString("wiredev.credits.5750")
+                    new NSString("wiredev.credits.24500")
+                |]
+            )
+        let request = new SKProductsRequest(ids, Delegate = productsRequestDelegate)
+        request.Start()
+
+
+
